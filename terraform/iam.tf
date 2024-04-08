eec4fe65cc74764341c9347cc15ddd9e6aeeabf1
AungMawMaw <aungmaw.bc@gmail.com>
2024-04-08 21:03:02 +0630

    finaltestxd

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,26 +1,40 @@
 ## NOTE: role - tasks execution role
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
-    actions = ["sts:AssumeRole"]
+    actions = ["sts:assumerole"]
     principals {
-      type        = "Service"
+      type        = "service"
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
 
-resource "aws_iam_role" "ecsTasksExecutionRole" {
+resource "aws_iam_role" "ecstasksexecutionrole" {
   name               = "${var.app_name}-ecs-tasks-execution-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 # policy 1 for ec2
 resource "aws_iam_role_policy_attachment" "ec2_policy" {
-  role       = aws_iam_role.ecsTasksExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+  role       = aws_iam_role.ecstasksexecutionrole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/amazonec2containerserviceforec2role"
 }
 
 # policy 2
+# data "aws_iam_policy_document" "twitter_service_access" { statement {
+#     effect = "allow"
+#     actions = [
+#       "dynamodb:describetable",
+#       "dynamodb:scan",
+#       "dynamodb:updateitem",
+#       "sqs:*"
+#     ]
+#     resources = [
+#       "arn:aws:sqs: ${var.aws_region}:${local.account_id}:${var.sqs_que_name}",
+#       "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${var.dynamo_vendor_table_name}"
+#     ]
+#   }
+# }
 data "aws_iam_policy_document" "twitter_service_access" {
   statement {
     effect = "Allow"
@@ -31,11 +45,12 @@ data "aws_iam_policy_document" "twitter_service_access" {
       "sqs:*"
     ]
     resources = [
-      "arn:aws:sqs: ${var.aws_region}:${local.account_id}:${var.sqs_que_name}",
+      "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_que_name}",
       "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${var.dynamo_vendor_table_name}"
     ]
   }
 }
+
 resource "aws_iam_policy" "twitter_service_access" {
   name        = "${var.app_name}-twitter_service_access"
   policy      = data.aws_iam_policy_document.twitter_service_access.json
@@ -43,5 +58,5 @@ resource "aws_iam_policy" "twitter_service_access" {
 }
 resource "aws_iam_role_policy_attachment" "attach_twitter_service_access_policy" {
   policy_arn = aws_iam_policy.twitter_service_access.arn
-  role       = aws_iam_role.ecsTasksExecutionRole.name
+  role       = aws_iam_role.ecstasksexecutionrole.name
 }
